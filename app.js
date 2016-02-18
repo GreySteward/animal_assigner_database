c0a898eee3c62651e220a07d8e0aef1bdbcb79f0
Chris Gibson <cigibson33@gmail.com>
2016-02-18 11:53:21 -0600

    datbase functionality

--- app.js
+++ app.js
@@ -16,10 +16,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 // get data route
-app.get('/person', function(req, res) {
+app.get('/people', function(req, res) {
     var results = [];
     pg.connect(connectionString, function(err, client, done) {
-        var query = client.query('SELECT * FROM people ORDER BY id DESC;');
+        var query = client.query('SELECT * FROM people ORDER BY person_id ASC;');
 
         // Stream results back one row at a time
         query.on('row', function(row) {
@@ -38,15 +38,38 @@ app.get('/person', function(req, res) {
     });
 });
 
+app.get('/animal', function(req, res) {
+    var results = [];
+    pg.connect(connectionString, function(err, client, done) {
+        var query = client.query('SELECT * FROM animal ORDER BY animal_id ASC;');
+
+        // Stream results back one row at a time
+        query.on('row', function(row) {
+            results.push(row);
+        });
+
+        // close connection
+        query.on('end', function() {
+            client.end();
+            return res.json(results);
+        });
+
+        if(err) {
+            console.log(err);
+        }
+    });
+});
+
+
 app.post('/people', function(req, res) {
     var addPerson = {
-        name: req.body.name,
-        address: req.body.address
+        first_name: req.body.first_name,
+        last_name: req.body.last_name
     };
 
     pg.connect(connectionString, function(err, client, done) {
-        client.query("INSERT INTO person (first_name, last_name) VALUES ($1, $2) RETURNING id",
-            [addPerson.name, addPerson.address],
+        client.query("INSERT INTO people (first_name, last_name) VALUES ($1, $2) RETURNING person_id",
+            [addPerson.first_name, addPerson.last_name],
             function (err, result) {
                 done();
                 if(err) {
@@ -58,6 +81,50 @@ app.post('/people', function(req, res) {
             });
     });
 
+});
+
+app.post('/animal', function(req, res) {
+    var addAnimal = {
+  //      animal_id: req.body.animal_id,
+        animal_name: req.body.animal_name,
+        animal_color: req.body.animal_color
+    };
+
+    pg.connect(connectionString, function(err, client, done) {
+        client.query("INSERT INTO animal (animal_name, animal_color) VALUES ($1, $2) RETURNING animal_id",
+            [addAnimal.animal_name, addAnimal.animal_color],
+            function (err, result) {
+                done();
+                if(err) {
+                    console.log("Error inserting data: ", err);
+                    res.send(false);
+                } else {
+                    res.send(result);
+                }
+            });
+    });
+
+});
+
+app.post('/addAnimalId', function(req, res) {
+    var addAnimalId = {
+        person_id: req.body.person_id,
+        animal_id: req.body.animal_id
+    };
+
+    pg.connect(connectionString, function(err, client, done) {
+        client.query('UPDATE person_id SET animal_id = VALUES ($1)',
+            [addAnimalId.animal_id],
+            function (err, result) {
+                done();
+                if(err) {
+                    console.log("Error inserting data: ", err);
+                    res.send(false);
+                } else {
+                    res.send(result);
+                }
+            });
+    });
 });
 
 app.get('/*', function(req, res) {
--- public/scripts/clientapp.js
+++ public/scripts/clientapp.js
@@ -1,23 +1,30 @@
 $(document).ready(function() {
 
-    $('#submit-button').on('click', postData);
-
+    $('#submit-people').on('click', postData);
+    $('#submit-animal').on('click', postAnimalData);
+    $('#magic').on('click', addAnimalId);
+    getData();
+    getAnimalData();
 
 });
+
 
 function postData() {
     event.preventDefault();
 
     var values = {};
-    $.each($('#sql-form').serializeArray(), function(i, field) {
+    $.each($('#people-form').serializeArray(), function(i, field) {
         values[field.name] = field.value;
     });
 
     console.log(values);
 
+    $('#people-form').find('input[type=text]').val('');
+
+
     $.ajax({
         type: 'POST',
-        url: '/person',
+        url: '/people',
         data: values,
         success: function(data) {
             if(data) {
@@ -32,12 +39,99 @@ function postData() {
 
 }
 
-function getData() {
+function postAnimalData() {
+    event.preventDefault();
+
+    var values = {};
+    $.each($('#animal-form').serializeArray(), function(i, field) {
+        values[field.name] = field.value;
+    });
+
+    console.log(values);
+
+    $('#animal-form').find('input[type=text]').val('');
+
+
     $.ajax({
-        type: 'GET',
-        url: '/people',
+        type: 'POST',
+        url: '/animal',
+        data: values,
+        success: function(data) {
+            if(data) {
+                // everything went ok
+                console.log('from server:', data);
+                getAnimalData();
+            } else {
+                console.log('error');
+            }
+        }
+    });
+}
+
+function addAnimalId() {
+    event.preventDefault();
+
+    var values = {};
+    $.each($('#animal_id').serializeArray(), function(i, field) {
+        values[field.name] = field.value;
+    });
+
+    console.log(values);
+
+    $('#animal-form').find('input[type=text]').val('');
+
+    $.ajax({
+        type: 'POST',
+        url: '/addAnimalId',
+        data: values,
         success: function(data) {
             console.log(data);
         }
     });
 }
+
+function getData() {
+    $('.person-data').remove();
+    $.ajax({
+        type: 'GET',
+        url: '/people',
+        success: function(data) {
+            console.log(data);
+            appendPeople(data);
+        }
+    });
+}
+
+function getAnimalData() {
+    $('.animal-data').remove();
+    $.ajax({
+        type: 'GET',
+        url: '/animal',
+        success: function(data) {
+            console.log(data);
+            appendAnimal(data);
+        }
+    });
+}
+
+
+function appendPeople(peopleData) {
+    for (var person in peopleData) {
+        var per = peopleData[person];
+        $('#person-data').append('<div class="person-match"></div>');
+        var $el = $('#person-data').children().last();
+
+        $el.append('<div class="person-data">' + per.first_name + " " + per.last_name + '</div>');
+        $el.append('<input type="text" id="animal_id" name="animal_id" />');
+        $el.append('<button id="magic">Magic</button>');
+    }
+}
+
+function appendAnimal(animalData) {
+    for (var animal in animalData) {
+        var ani = animalData[animal];
+        $('#animal-data').append('<p class="animal-data">' + ani.animal_id + " " + ani.animal_name + " " + ani.animal_color + '</p>')
+}
+}
+
+
